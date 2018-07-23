efa9ec4f032057a5a5ab9b4fadd1a2e6d3833ded
Florentin HAUTON <florentin.hauton@hotmail.fr>
2018-07-23 11:45:44 +0200

    add MovieList component

---
+++ front/src/components/MovieList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MovieList extends React.Component {
+  render () {
+    return (
+      <div>
+        MovieList
+      </div>
+    )
+  }
+}
+
+export default MovieList
